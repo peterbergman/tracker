92c6cb16d64e5ebd804d94621cea32eb75eb9c26
Peter Bergman <peter.bergman@live.se>
2015-02-09 19:44:49 +0100

    Added more url routing for api

--- tracker/tracker/urls.py
+++ tracker/tracker/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^datahandler/([^/]+)/([^/]+)/$', 'datahandler.views.delegate_request'),
-    url(r'^api/$', 'api.views.delegate_request'),
+    url(r'^api/accounts/([^/]+)$', 'api.views.account'),
+    url(r'^api/accounts/([^/]+)/sites/([^/]+)/start_date/([^/]+)/end_date/([^/]+)$', 'api.views.site'),
 )
