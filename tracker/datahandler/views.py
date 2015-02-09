8e9711a32d83ecdca064fd23dd5514e89900f7d7
Peter Bergman <peter.bergman@live.se>
2015-02-09 11:26:31 +0100

    Added method to generate pixel data

--- tracker/datahandler/views.py
+++ tracker/datahandler/views.py
@@ -49,7 +49,11 @@ def parse_user_agent(request):
 
 def create_response(visitor_id):
     """Create the HttpResponse that sets the visitor_id cookie when sent back to the client."""
-    response = HttpResponse()
+    response = HttpResponse(content_type='image/gif')
     expire_time = datetime.datetime.fromtimestamp(time.time() + 157784630)
     response.set_cookie('visitor_id', value=visitor_id, expires=expire_time)
+    response.content = generate_pixel()
     return response
+
+def generate_pixel():
+    return '\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00\x21\xf9\x04\x01\x00\x00\x00\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b'
