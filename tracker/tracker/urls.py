156c7091c47fdc211c77d3111e054009528c2500
Peter Bergman <peter.bergman@live.se>
2015-02-09 10:47:17 +0100

    Updated URL routing to for datahandler

--- tracker/tracker/urls.py
+++ tracker/tracker/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^datahandler/$', 'datahandler.views.delegate_request'),
+    url(r'^datahandler/([^/]+)/([^/]+)/$', 'datahandler.views.delegate_request'),
 )
