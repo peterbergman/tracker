622e94f8d0538884d40cd7c2c4e304012bb4ddd1
Peter Bergman <peter.bergman@live.se>
2015-02-10 11:38:46 +0100

    WIP Added aggregation logic for browsers report

--- tracker/api/views.py
+++ tracker/api/views.py
@@ -27,14 +27,14 @@ def page_views(request, account_id, site_id, start_date, end_date):
         }
       },
       {
-        '$project': # in the aggregation project, use page url field and a new field called date which only containts YYYY-MM-DD
+        '$project': # in the aggregation project, use page_url field and a new field called date which only containts YYYY-MM-DD
         {
           'page_url': 1,
           'date': {'$substr': ["$time", 0, 10] }
         }
       },
       {
-        '$group': # first create a grouping on page_url AND date, then for each group, create an array of dates where its been hit (multiple hits during one day are represented with multiple entries in the array)
+        '$group': # first create a grouping on page_url AND date
         {
           '_id':
           {
@@ -43,7 +43,7 @@ def page_views(request, account_id, site_id, start_date, end_date):
           },
           'dates':
           {
-            '$push': '$date'
+            '$push': '$date' # for each group, create an array of dates where the page been hit (multiple hits during one day are represented with multiple entries in the array)
           },
           'hits':
           {
@@ -89,14 +89,14 @@ def visitors(request, account_id, site_id, start_date, end_date):
         }
       },
       {
-        '$project':
+        '$project': # in the aggregation project, use visitor_id field and a new field called date which only containts YYYY-MM-DD
         {
           'visitor_id': 1,
           'date': {'$substr': ["$time", 0, 10] }
         }
       },
       {
-        '$group':
+        '$group': # first group on dates and for each date, make a set of all visitor_id values that had an event reqistered that date
         {
           '_id': '$date',
           'visitors':
@@ -109,15 +109,15 @@ def visitors(request, account_id, site_id, start_date, end_date):
         }
       },
       {
-        '$unwind': '$visitors'
+        '$unwind': '$visitors' # unwind the visitors set so that the next step can count all visitor_id values for each date
       },
       {
-        '$group':
+        '$group': # again group on date
         {
           '_id': '$_id',
           'visitors':
           {
-            '$sum': 1
+            '$sum': 1 # for each group, sum all the visitor_id values in that group which is the visitor count for that date
           }
         }
       }
@@ -127,3 +127,75 @@ def visitors(request, account_id, site_id, start_date, end_date):
     for date in result['result']:
         response['sites'][0]['dates'].append({date['_id'] : {'visitors' : date['visitors']}})
     return HttpResponse(dumps(response), content_type='application/json')
+
+def browsers(request, account_id, site_id, start_date, end_date):
+    result = settings.DB.command('aggregate', 'event', pipeline=[
+    {
+      '$match': # only match dates within the given interval
+      {
+        'time':
+        {
+          '$gte': datetime.strptime(start_date, '%Y-%m-%d'),
+          '$lt':  datetime.strptime(end_date, '%Y-%m-%d')
+        }
+      }
+    },
+    {
+    '$project':
+    {
+    'user_agent': 1,
+    'visitor_id': 1,
+    'date': {'$substr': ["$time", 0, 10] }
+    }
+    },
+    {
+      '$group': # first create a grouping on user_agent AND visitor_id
+      {
+        '_id':
+        {
+          'user_agent':'$user_agent',
+          'visitor_id' : '$visitor_id'
+        },
+        'dates':
+        {
+          '$addToSet': '$date' # for each group, create an array of dates where the combination of user_agent/visitor_id has been seen (multiple hits during one day are represented with multiple entries in the array)
+        }
+      }
+    },
+    {
+    '$unwind': '$dates' # unwind the date array so that it can be used to group on in the next step
+    },
+    {
+    '$group': # group on dates
+    {
+    '_id': '$dates',
+    'browsers':
+    {
+    '$addToSet': '$_id.user_agent' # for each date, make a set of user_agent values
+    }
+    }
+    },
+    {
+    '$unwind' : '$browsers' # unwind the set of user_agent values so that it can be used in the next step to make a count of the number of different user_agent values per date
+    },
+    {
+    '$group': # group on date again
+    {
+    '_id': '$_id',
+    'browsers':
+    {
+    '$addToSet' : '$browsers' # add each user_agent to a set
+    },
+    'total':
+    {
+    '$sum': 1 # make a sum for all user_agent values per date which will be the count of unique user_agent for each date
+    }
+    }
+    }
+    ])
+
+    response = {'account_id' : account_id, 'sites' : [{'site_id' : site_id, 'site_name' : 'None', 'dates' : []}]}
+    #for date in result['result']:
+        #response['sites'][0]['dates'].append({date['_id'] : {'browsers' : {'total' : date['total'], 'browsers' : date['browsers']}}})
+
+    return HttpResponse(dumps(result), content_type='application/json')
