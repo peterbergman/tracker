6e6f75d2c63556b7e3485b4680838d18eb3fe5a4
Peter Bergman <peter.bergman@live.se>
2015-02-10 15:01:10 +0100

    Updated the url routing for the account api

--- tracker/tracker/urls.py
+++ tracker/tracker/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^datahandler/([^/]+)/([^/]+)/$', 'datahandler.views.delegate_request'),
-    url(r'^api/accounts/([^/]+)$', 'api.views.account'),
+    url(r'^api/accounts/([^/]+)$', 'api.views.get_account'),
+    url(r'^api/accounts/$', 'api.views.create_account'),
     url(r'^api/accounts/([^/]+)/sites/([^/]+)/start_date/([^/]+)/end_date/([^/]+)$', 'api.views.site'),
     url(r'^api/accounts/([^/]+)/sites/([^/]+)/start_date/([^/]+)/end_date/([^/]+)/page_views$', 'api.views.page_views'),
     url(r'^api/accounts/([^/]+)/sites/([^/]+)/start_date/([^/]+)/end_date/([^/]+)/visitors$', 'api.views.visitors'),
