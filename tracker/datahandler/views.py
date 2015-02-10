1e0b9dee9503fbb028da43fafa8aa4149e6087ea
Peter Bergman <peter.bergman@live.se>
2015-02-10 10:11:34 +0100

    Changed so that an instance of the Event class is created instead of a dict, then __dict__ is called on that object before using it in the DB insert

--- tracker/datahandler/views.py
+++ tracker/datahandler/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
-#from datahandler.models import Event
+from datahandler.models import Event
 from django.conf import settings
 import datetime
 import time
 import uuid
-import bson
 
 def delegate_request(request, account_id, site_id):
     """Delegate a request by calling the correct handler."""
@@ -22,17 +21,8 @@ def handle_get(request, account_id, site_id):
     page_url = parse_page_url(request)
     user_agent = parse_user_agent(request)
     time = parse_time(request)
-    #event = Event(account_id=account_id, site_id=site_id, visitor_id=visitor_id, page_url=page_url, user_agent=user_agent)
-    event = {
-        '_id' : bson.ObjectId(),
-        'time' : datetime.datetime.now(),
-        'visitor_id' : visitor_id,
-        'account_id' : account_id,
-        'site_id' : site_id,
-        'page_url' : page_url,
-        'user_agent' : user_agent
-    }
-    settings.DB.event.insert(event, w=0)
+    event = Event(account_id=account_id, site_id=site_id, visitor_id=visitor_id, page_url=page_url, user_agent=user_agent)
+    settings.DB.event.insert(event.__dict__, w=0)
 
     return create_response(visitor_id)
 
