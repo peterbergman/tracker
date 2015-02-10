a5b58955295132ddfcf5c5065dca0cd43c8c9e1c
Peter Bergman <peter.bergman@live.se>
2015-02-10 11:38:32 +0100

    Added url routing for browsers report in api

--- tracker/tracker/urls.py
+++ tracker/tracker/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^api/accounts/([^/]+)/sites/([^/]+)/start_date/([^/]+)/end_date/([^/]+)$', 'api.views.site'),
     url(r'^api/accounts/([^/]+)/sites/([^/]+)/start_date/([^/]+)/end_date/([^/]+)/page_views$', 'api.views.page_views'),
     url(r'^api/accounts/([^/]+)/sites/([^/]+)/start_date/([^/]+)/end_date/([^/]+)/visitors$', 'api.views.visitors'),
+    url(r'^api/accounts/([^/]+)/sites/([^/]+)/start_date/([^/]+)/end_date/([^/]+)/browsers$', 'api.views.browsers'),
 
 )
