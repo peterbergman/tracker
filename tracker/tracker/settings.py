bffb7df796815e090d82144bc2f8c9bb2af31c83
Peter Bergman <peter.bergman@live.se>
2015-02-10 20:12:21 +0100

    Added front-app and static file handling

--- tracker/tracker/settings.py
+++ tracker/tracker/settings.py
@@ -8,7 +8,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
-#from mongoengine import *
 import pymongo
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -27,6 +26,10 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates/'),
+)
+
 ALLOWED_HOSTS = []
 
 
@@ -41,6 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'datahandler',
     'api',
+    'front',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -97,3 +101,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(
+        os.path.dirname(__file__),
+        'static',
+    ),
+)
