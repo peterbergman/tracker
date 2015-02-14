7c6132e02e880daada1879fc28e51c33d03d577f
Peter Bergman <peter.bergman@live.se>
2015-02-14 15:54:02 +0100

    Removed comments

--- tracker/front/static/js/Tracker.js
+++ tracker/front/static/js/Tracker.js
@@ -1,12 +1,2 @@
-/*
-
-included with tag:
-
-(function () {
-        var tracker = document.createElement('script');
-        tracker.type = 'text/javascript';
-        tracker.async = true;
-        tracker.src = http url to script;
-        var a = document.getElementsByTagName('script')[0];
-        a.parentNode.insertBefore(tracker, a);
-    } ());*/(function(){  var pixel = new Image();  pixel.src = 'http://localhost:8000/datahandler/' + tracker.accountId + '/' + tracker.siteId + '/?page_url=' + (tracker.pageUrl ? tracker.pageUrl : encodeURIComponent(document.location.href));})()
+(function(){
+  var pixel = new Image();  pixel.src = 'http://localhost:8000/datahandler/' + tracker.accountId + '/' + tracker.siteId + '/?page_url=' + (tracker.pageUrl ? tracker.pageUrl : encodeURIComponent(document.location.href));})()
