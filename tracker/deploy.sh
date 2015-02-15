85dbc7f130bcd4e92826cadccc6ffbb6d00de181
Peter Bergman <peter.bergman@live.se>
2015-02-15 13:10:33 +0100

    deploy script update

--- tracker/deploy.sh
+++ tracker/deploy.sh
@@ -7,10 +7,10 @@ sudo rm -r /var/www/tracker/helper
 sudo rm -r /var/www/tracker/front
 sudo rm -r /var/www/tracker/templates
 
-sudo cp -r api/ /var/www/tracker/
-sudo cp -r datahandler/ /var/www/datahandler/
-sudo cp -r helper/ /var/www/helper/
-sudo cp -r front/ /var/www/front/
-sudo cp -r templates/ /var/www/templates/
+sudo cp -r api /var/www/tracker
+sudo cp -r datahandler /var/www/datahandler
+sudo cp -r helper /var/www/helper
+sudo cp -r front /var/www/front
+sudo cp -r templates /var/www/templates
 
 sudo /etc/init.d/apache2 reload
