4566d76ed093d283fe101d8a11ce421c6747a9ba
Peter Bergman <peter.bergman@live.se>
2015-02-16 12:48:28 +0100

    Changed data handler url to EC2

--- tracker/front/static/js/tracker.js
+++ tracker/front/static/js/tracker.js
@@ -1,4 +1,4 @@
 (function(){
   var pixel = new Image();
-  pixel.src = 'http://localhost:8000/datahandler/' + tracker.accountId + '/' + tracker.siteId + '/?page_url=' + (tracker.pageUrl ? tracker.pageUrl : encodeURIComponent(document.location.href));
+  pixel.src = 'http://http://ec2-54-172-75-176.compute-1.amazonaws.com/datahandler/' + tracker.accountId + '/' + tracker.siteId + '/?page_url=' + (tracker.pageUrl ? tracker.pageUrl : encodeURIComponent(document.location.href));
 })()
