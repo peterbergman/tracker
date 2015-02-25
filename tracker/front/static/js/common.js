358677c8e65e99cf3f51dbbf7c8f733418c08bbf
Peter Bergman <peter.bergman@live.se>
2015-02-25 15:10:53 +0100

    Datepicker lib include

--- tracker/front/static/js/common.js
+++ tracker/front/static/js/common.js
@@ -1,7 +1,12 @@
 requirejs.config({
   baseUrl: '/static/js',
-  shim : {
-    "bootstrap" : { "deps" :['jquery'] }
+  shim: {
+    bootstrap: {
+      deps: ['jquery']
+    },
+    datepicker: {
+      deps: ['jquery']
+    }
   },
   paths: {
     jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min',
@@ -10,6 +15,7 @@ requirejs.config({
     chartjs: 'lib/chart',
     constants: 'constants',
     helpers: 'helpers',
-    base64: 'lib/base64'
+    base64: 'lib/base64',
+    datepicker: 'lib/bootstrap-datepicker'
   }
 })
